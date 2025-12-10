b1985908257806130211a0bba7bed6795b9dd31b
Apurva Jadhav <apurvajadhav566@gmail.com>
2025-12-10 16:50:46 +0530

    geometery

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def area (len,br):
+    print("area :",(len*br))
+
+def peri(len,br):
+    print("perimeter :",2*(len+br))   
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import arthmatic
-
+import geometry as ge
 
 
 print("hello Apurva")
 arthmatic.add(10,5)
 arthmatic.sub(10,5)
+
+ge.area(10,2)
+ge.peri(10,2)
